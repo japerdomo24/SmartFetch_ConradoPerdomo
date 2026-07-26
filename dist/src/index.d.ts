b721721d5ff8ecbd5ca6fa7a5799a0d3e121702b
japerdomo24 <japerdomo.24@est.ucab.edu.ve>
2026-07-26 04:04:51 -0400

    Delete dist directory

--- dist/src/index.d.ts
+++
@@ -1,4 +0,0 @@
-export { SmartFetch } from './SmartFetch.js';
-export type { SmartFetchConfig, HttpResponse } from './types/index.js';
-export { SmartFetchError } from './errors/SmartFetchError.js';
-//# sourceMappingURL=index.d.ts.map
--- dist/src/index.js
+++
@@ -1,3 +0,0 @@
-export { SmartFetch } from './SmartFetch.js';
-export { SmartFetchError } from './errors/SmartFetchError.js';
-//# sourceMappingURL=index.js.map
